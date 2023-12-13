d40fd95f3985ebe12311ee512b56b2fc66038849
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2023-12-13 23:05:06 +0100

    commit simple

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,3 +1,8 @@
 // console.log("test connection script")
 
-const url = "http://localhost:5678/api/works"
+const url = "http://localhost:5678/api/works";
+const gallery = document.querySelector(".gallery");
+
+
+
+const getWorks =
