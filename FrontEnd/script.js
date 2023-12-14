c6f090cf052a68bb1d7616abd0859c49ff7003ac
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2023-12-14 18:06:01 +0100

    Ajout des "works" en javascript

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,4 +5,23 @@ const gallery = document.querySelector(".gallery");
 
 
 
-const getWorks =
+const getWorks = () => {
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+
+        .then(function (data) {
+            console.log(data)
+
+            for (works in data) {
+                gallery.innerHTML += `<figure>
+            <img src="${data[works].imageUrl}" alt="${data[works].title}">
+            <figcaption>${data[works].title}</figcaption>
+        </figure>`
+            }
+
+        })
+}
+
+getWorks()
