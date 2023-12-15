f7cf96aaf42ff6fac5312c73b027567edf42866c
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2023-12-15 17:01:17 +0100

    commit save

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,6 @@
 
 // On récupère l'dresse de l'API et on la met dans une variable
 const urlApi = "http://localhost:5678/api/works";
-
 const gallery = document.querySelector(".gallery");
 
 
@@ -16,12 +15,22 @@ async function getWorks() {
 
         .then(function (data) {
             console.log(data)
+
             for (works in data) {
-                gallery.innerHTML += `<figure data-${data[works].category.name} class="active">
+                gallery.innerHTML += `<figure data-${data[works].category.id} class="active">
+                
             <img src="${data[works].imageUrl}" alt="${data[works].title}">
             <figcaption>${data[works].title}</figcaption>
         </figure>`
             }
+            /*     CONSEILS MENTOR
+                 for (works in data) {
+                    gallery.innerHTML += `<figure data-category="${data[works].category.id}" class="active">
+                    
+                <img src="${data[works].imageUrl}" alt="${data[works].title}">
+                 <figcaption>${data[works].title}</figcaption>
+             </figure>`
+                 } */
         })
 }
 // on lance la fonction
@@ -37,20 +46,26 @@ et qui renvoi la valeur de son id */
 
 for (let filtre of filtres) {
     filtre.addEventListener("click", function () {
-        let tag = this.id;
+        let tag = this.id; // donne la valeur à tag -> l'id de la div ex:"tous"
         //console.log(tag)
 
 
         let travaux = document.querySelectorAll(".gallery figure");
-/* Avec une boucle, on inqidue que les div figure sont inactive en remplaçant leur class */
+        /* Avec une boucle, on inqidue que les div figure sont inactive en remplaçant leur class */
         for (let travail of travaux) {
             travail.classList.replace("active", "inactive");
+            console.log(travail,tag)
 
-/* Ensuite on dit que lorsque l'etiquette correspond au dataset on remplace la class par active
-et on definit aussi une valeur pour l'etiquette "tous" */
+            /* Ensuite on dit que lorsque l'id de tag correspond à celui dans le dataset on remplace la class par active
+            et on definit aussi une valeur pour l'etiquette "tous" */
+
             if (tag in travail.dataset || tag === "tous") {
                 travail.classList.replace("inactive", "active");
             }
+            // CONSEILS MENTOR
+            // if (travail.getAttribute("data-category.id") === tag || tag === "tous") {
+            //     travail.classList.replace("inactive", "active");
+            // }
         }
     });
 }
