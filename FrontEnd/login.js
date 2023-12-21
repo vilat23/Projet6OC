94638570e63f8de8b71b8becccadc43a749d9ef5
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2023-12-21 18:21:20 +0100

    creation du fichier js pour la page login

---
+++ FrontEnd/login.js
@@ -0,0 +1,10 @@
+
+// On définit differentes variables, les champs input du formulaire 
+
+const emailInput = document.querySelector("form #email")
+const passwordInput = document.querySelector("form #password")
+const form = document.querySelector("form")
+
+// Variable du champ de texte pour l'affichage du message d'erreur
+
+const errorMsgField = document.querySelector("#formSection p")
