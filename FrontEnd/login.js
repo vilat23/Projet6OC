d61cc196cccae3ab34053253fcf8b34af30b9b8b
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2023-12-27 17:30:47 +0100

    re edit

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,18 +1,18 @@
 
 // On définit differentes variables, le chemin API de users, les champs input du formulaire 
 const urlUsers = "http://localhost:5678/api/users/login";
+const form = document.querySelector("form");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const form = document.querySelector("form");
 
 // Variable du champ de texte pour l'affichage du message d'erreur
 const errorMsgField = document.querySelector("#formSection p");
 
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (event) => {
 
     // empêche le rechargement de la page au submit
-    e.preventDefault()
+    event.preventDefault()
 
     let logInfos = {
         email: emailInput.value,
@@ -31,15 +31,15 @@ form.addEventListener('submit', (e) => {
             // console.log(response.ok); si c'est ok on renvoie la reponse en json
             if(response.ok) {
                 response.json()
-            // fonction pur vérifier que le token est bien présent dans le local storage,
-            // si c'est ok on renvoie vers la page admin
+            // fonction pour vérifier que les donnees de connexions entrées dans le local storage correspondent  
+            // à celles du token, si c'est ok on renvoie vers la page admin.html
               .then((data) => {
-                const userdata = data.token;
-                if (localStorage.user = userdata) 
+                const userToken = data.token;
+                if (localStorage.logInfos = userToken) 
                 document.location.href=("admin.html"); 
             // sinon on affiche un message d'erreur dans le champ dédié
                 })} else {
-                  document.querySelector("#formSection p").innerHTML = "Erreur dans l’identifiant ou le mot de passe";
+                    errorMsgField.innerHTML = "Erreur dans l’identifiant ou le mot de passe";
                 }
 
         })
