357aa1e603a5137d72c08b833be5fc3db51b1576
Phimanao Paul <142014758+vilat23@users.noreply.github.com>
2024-01-04 01:04:46 +0100

    edit code login pour stockage token + edit code modal suppression

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -4,8 +4,6 @@ const form = document.querySelector("form");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 
-const allTrashIcons = []
-
 // Variable du champ de texte pour l'affichage du message d'erreur
 const errorMsgField = document.querySelector("#formSection p");
 
@@ -32,82 +30,37 @@ form.addEventListener("submit", (event) => {
         response
           .json()
           // fonction pour vérifier que les donnees de connexions entrées dans le local storage correspondent
-          // à celles du token, si c'est ok on renvoie vers la page admin.html
+          // à celles du token, si c'est ok on stock le token et le userId et on renvoie vers la page admin.html
           .then((data) => {
-            const userToken = data.token;
-            if ((localStorage.logInfos = userToken))
+            // console.log(data)
+            if (data.token) {
+              localStorage.setItem("token", data.token);
+              localStorage.setItem("userId", data.userId);
+              // console.log(data.token)
+              // console.log(data.userId)
               document.location.href = "admin.html";
-            // sinon on affiche un message d'erreur dans le champ dédié
+            }
           });
-      } else {
+      }
+      // sinon on affiche un message d'erreur dans le champ dédié
+      else {
         errorMsgField.innerHTML =
           "Erreur dans l’identifiant ou le mot de passe";
       }
     });
 });
 
-// ----------------------------------------------
-// -----------------PARTIE MODAL---------------
-// ------------------------------------------------
+// ---------Gestion du logout----------------
 
-// AFFICHAGE ET FERMETURE DE LA MODAL
-//variables
-const editBtn = document.querySelector(".editBtn");
-const modalContainer = document.querySelector(".modalContainer");
-const closingCross = document.querySelector(".fa-xmark");
-
-//FONCTION POUR l'AFFICHAGE ET FERMETURE DE LA MODAL-------------------------------
-
-function modalSettings() {
-  // Affichage de la modal au click sur "modifier" en mettant le style en "display flex"
-  editBtn.addEventListener("click", () => {
-    console.log(editBtn);
-    modalContainer.style.display = "flex";
-  });
-
-  // Fermeture de la modal au click sur "la croix" en mettant le style en "display none"
-  closingCross.addEventListener("click", () => {
-    modalContainer.style.display = "none";
-  });
-  // Fermeture de la modal au click à l'extérieur de la modal en mettant le style en "display none"
-  modalContainer.addEventListener("click", (event) => {
-    if (event.target.className === "modalContainer") {
-      modalContainer.style.display = "none";
-    }
-  });
-}
-modalSettings();
-
-// INSERTION DES PHOTOS DANS LA GALLERY MODAL-------------------------
-
-const modalProjects = document.querySelector(".modalProjects");
-// console.log(modalProjects);
-
-async function getWorks() {
-  const reponse = await fetch(urlApiWorks);
-  return (
-    reponse
-      .json()
-
-      .then(function (data) {
-        for (works in data) {
-            
-          modalProjects.innerHTML += `<figure data-id="${data[works].category.name}">    
-              <img src="${data[works].imageUrl}" alt="${data[works].title}">
-              <span class="trashIcon"><i class="fa-solid fa-trash-can fa-xs"></i></span>
-          </figure>`;
-        }
-        const trashIcons = document.querySelectorAll(".trashIcon");
-        console.log(trashIcons);
-        allTrashIcons.push(trashIcons);
-      })
-  );
-
-}
-getWorks();
-
-//Fonction pour supprimer les photos-----------------------
-function deletePhoto() {
-    console.log(allTrashIcons);
-}
-deletePhoto();
+const logoutLink = document.querySelector(".logoutLink");
+// console.log(logoutLink)
+logoutLink.addEventListener("click", () => {
+  function logout() {
+    // Supprime le userId et le token du localStorage
+    localStorage.removeItem("userId");
+    localStorage.removeItem("token");
+    // Redirige l'utilisateur vers la page d'accueil
+    window.location.href = "index.html";
+  }
+  logout();
+});
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -9,26 +9,26 @@ const filtreSection = document.querySelector("#filtres");
 
 // Fonction Fetch pour récupérer des données dans l'Api et les retourner en json
 async function getWorks() {
-  const reponse = await fetch(urlApiWorks);
-  return (
-    reponse
-      .json()
+    const reponse = await fetch(urlApiWorks);
+    return (
+        reponse
+            .json()
 
-      /* Ensuite on insère ces données dans data et on insert les balises <figure>
-         contenant les travaux et leurs données respectives, dans le html à l'intérieur de la div ayant la classe .gallery */
+            /* Ensuite on insère ces données dans data et on insert les balises <figure>
+               contenant les travaux et leurs données respectives, dans le html à l'intérieur de la div ayant la classe .gallery */
 
-      .then(function (data) {
-        console.log(data);
+            .then(function (data) {
+                // console.log(data);
 
-        for (works in data) {
-          gallery.innerHTML += `<figure data-id="${data[works].category.name}" class="active">
+                for (works in data) {
+                    gallery.innerHTML += `<figure data-id="${data[works].category.name}" class="active">
                 
             <img src="${data[works].imageUrl}" alt="${data[works].title}">
             <figcaption>${data[works].title}</figcaption>
         </figure>`;
-        }
-      })
-  );
+                }
+            })
+    );
 }
 // on lance la fonction
 getWorks();
@@ -37,101 +37,149 @@ getWorks();
 
 // ------ AJOUT DES FILTRES EN JAVASCRIPT-----------
 async function getFilters() {
-  const reponse = await fetch(urlApiFilters);
-  return reponse
-    .json()
+    const reponse = await fetch(urlApiFilters);
+    return reponse
+        .json()
 
-    .then(function (dataCategories) {
-      for (categories in dataCategories) {
-        const filtreSection = document.querySelector("#filtres");
-        const filter = document.createElement("div");
-        filter.setAttribute("id", `${dataCategories[categories].name}`);
-        filter.innerHTML += `${dataCategories[categories].name}`;
-        filtreSection.appendChild(filter);
+        .then(function (dataCategories) {
+            // console.log(dataCategories)
+            for (categories in dataCategories) {
+                const filtreSection = document.querySelector("#filtres");
+                const filter = document.createElement("div");
+                filter.setAttribute("id", `${dataCategories[categories].name}`);
+                filter.innerHTML += `${dataCategories[categories].name}`;
+                filtreSection.appendChild(filter);
 
-        // ------ PARTIE FONCTION DE FILTRAGE-----------
-        let filtres = document.querySelectorAll("#filtres div");
-        for (let filtre of filtres) {
-          filtre.addEventListener("click", function () {
-            let tag = this.id; // donne la valeur à tag -> l'id de la div ex:"tous" ou "Appartement" etc...
-            // console.log(tag);
+                // ------ PARTIE FONCTION DE FILTRAGE-----------
+                let filtres = document.querySelectorAll("#filtres div");
+                for (let filtre of filtres) {
+                    filtre.addEventListener("click", function () {
+                        let tag = this.id; // donne la valeur à tag -> l'id de la div ex:"tous" ou "Appartement" etc...
+                        // console.log(tag);
 
-            let worksList = document.querySelectorAll(".gallery figure");
-            /* Avec une boucle, on indique que les div figure sont non-affichees en remplaçant
-              leur class active par inactive */
-            for (let eachWork of worksList) {
-              eachWork.classList.replace("active", "inactive");
-              console.log(eachWork, tag);
+                        let worksList = document.querySelectorAll(".gallery figure");
+                        /* Avec une boucle, on indique que les div figure sont non-affichees en remplaçant
+                          leur class active par inactive */
+                        for (let eachWork of worksList) {
+                            eachWork.classList.replace("active", "inactive");
+                            //   console.log(eachWork, tag);
 
-              /* Ensuite on dit que lorsque "SI" l'id de tag correspond à celui dans le dataset 
-                "OU" si ça correspond à "tous"
-                          -> on remplace la class inactive par active
-                          */
-              if (eachWork.dataset.id === tag || tag === "tous") {
-                eachWork.classList.replace("inactive", "active");
-              }
+                            /* Ensuite on dit que lorsque "SI" l'id de tag correspond à celui dans le dataset 
+                              "OU" si ça correspond à "tous"
+                                        -> on remplace la class inactive par active
+                                        */
+                            if (eachWork.dataset.id === tag || tag === "tous") {
+                                eachWork.classList.replace("inactive", "active");
+                            }
+                        }
+                    });
+                }
             }
-          });
-        }
-      }
-    });
+        });
 }
 // on lance la fonction
 getFilters();
 
-// ------------------------------------------------------------------
-// CODE VERSION 1
-// ------------------------------------------------------------------
+// ----------------------------------------------
+// -----------------PARTIE MODAL---------------
+// ------------------------------------------------
 
-// ------ AJOUT DES FILTRES EN JAVASCRIPT-----------
+// AFFICHAGE ET FERMETURE DE LA MODAL
+//variables
+const editBtn = document.querySelector(".editBtn");
+const modalContainer = document.querySelector(".modalContainer");
+const closingCross = document.querySelector(".fa-xmark");
 
-// async function getFilters() {
-//   const reponse = await fetch(urlApiFilters);
-//   return reponse
-//     .json()
+//FONCTION POUR l'AFFICHAGE ET FERMETURE DE LA MODAL-------------------------------
 
-//     .then(function (dataCategories) {
-//       for (categories in dataCategories) {
-//         const filtreSection = document.querySelector("#filtres");
-//         const filter = document.createElement("div");
-//         filter.setAttribute("id", `${dataCategories[categories].id}`);
-//         filter.innerHTML += `${dataCategories[categories].name}`;
-//         filtreSection.appendChild(filter);
+function modalSettings() {
+    // Affichage de la modal au click sur "modifier" en mettant le style en "display flex"
+    editBtn.addEventListener("click", () => {
+        // console.log(editBtn);
+        modalContainer.style.display = "flex";
+    });
 
-//         console.log(filter);
-//       }
-//     });
-// }
-// // on lance la fonction
-// getFilters();
+    // Fermeture de la modal au click sur "la croix" en mettant le style en "display none"
+    closingCross.addEventListener("click", () => {
+        modalContainer.style.display = "none";
+    });
+    // Fermeture de la modal au click à l'extérieur de la modal en mettant le style en "display none"
+    modalContainer.addEventListener("click", (event) => {
+        if (event.target.className === "modalContainer") {
+            modalContainer.style.display = "none";
+        }
+    });
+}
+modalSettings();
 
-// ------ PARTIE FONCTION DE FILTRAGE-----------
+// INSERTION DES PHOTOS DANS LA GALLERY MODAL-------------------------
 
-// On défini les différents filtres dans une variable
-// let filtres = document.querySelectorAll("#filtres div");
+const modalProjects = document.querySelector(".modalProjects");
+// console.log(modalProjects);
 
-/* Avec une boucle, on ajoute un eventListener au click pour chaque filtre
-et qui renvoi la valeur de son id */
+async function getModalWorks() {
+    const reponse = await fetch(urlApiWorks);
+    return reponse
+        .json()
 
-// for (let filtre of filtres) {
-//   filtre.addEventListener("click", function () {
-//     let tag = this.id; // donne la valeur à tag -> l'id de la div ex:"tous"
-//     console.log(tag);
+        .then((data) => {
+            // console.log(data);
+            data.forEach((element) => {
+                // console.log(element);
+                const modalFigure = document.createElement("figure");
+                const modalImg = document.createElement("img");
+                const modalSpan = document.createElement("span");
+                const modalIcon = document.createElement("i");
+                modalSpan.classList.add("trashIcon");
+                modalIcon.classList.add("fa-solid", "fa-trash-can", "fa-xs");
+                modalImg.src = element.imageUrl;
+                modalSpan.id = element.id;
 
-//     let worksList = document.querySelectorAll(".gallery figure");
+                modalSpan.appendChild(modalIcon);
+                modalFigure.appendChild(modalImg);
+                modalFigure.appendChild(modalSpan);
+                modalProjects.appendChild(modalFigure);
+            });
+            deleteWork();
+        });
+}
+getModalWorks();
 
-//     /* Avec une boucle, on indique que les div figure sont non-affichees en remplaçant leur class active par inactive */
-//     for (let eachWork of worksList) {
-//       eachWork.classList.replace("active", "inactive");
-//       console.log(eachWork, tag);
-
-//       /* Ensuite on dit que lorsque "SI" l'id de tag correspond à celui dans le dataset  "OU" si ça correspond à "tous"
-//             -> on remplace la class inactive par active
-//             */
-
-//       if (eachWork.dataset.id === tag || tag === "tous") {
-//         eachWork.classList.replace("inactive", "active");
-//       }
-//     }
-//   });
-// }
+function deleteWork() {
+    const token = localStorage.getItem("token");
+    // console.log(token)
+    const trashIcons = document.querySelectorAll(".trashIcon");
+    console.log(trashIcons);
+    trashIcons.forEach((icon) => {
+        icon.addEventListener("click", (event) => {
+            console.log(icon);
+            const trashIconid = icon.id;
+            console.log(trashIconid);
+            const deleteConfirm = confirm(
+                "Etes vous sur de vouloir supprimer le projet ?"
+            );
+            if(deleteConfirm){
+                // function trashDelete(workId){
+                //     fetch("http://localhost:5678/api/works/" + workId, {
+                //         method: "DELETE",
+                //         headers: {
+                //             "Content-Type": "application/json",
+                //             Authorization: "Bearer" + localStorage.getItem("token"),
+                //         },
+                //     })
+                //     .then((response) =>{
+                //         if(!response.ok){
+                //             throw new Error("La supression à échoué");
+                //         }
+                //         console.log(response)
+                //         return response.json();
+                //     })
+                // }
+                // trashDelete(workId)
+                // si ok Appelle la fonction de suppression
+                console.log(trashIconid)
+            }
+            
+        });
+    });
+}
